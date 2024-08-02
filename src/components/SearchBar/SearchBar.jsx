21f48a9ed04ee4494b0503796f95a2ca9aa87af9
Unn2007 <unn2007@i.ua>
2024-08-02 12:47:51 +0300

    upd

---
+++ src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,19 @@
+
+
+function SearchBar(onSubmit) {
+return (
+<header>
+  <form>
+    <input
+      type="text"
+      autocomplete="off"
+      autofocus
+      placeholder="Search images and photos"
+    />
+    <button type="submit">Search</button>
+  </form>
+</header>
+
+);
+
+}
--- src/utils/images-api.js
+++ src/utils/images-api.js
@@ -2,8 +2,32 @@ import axios from "axios";
 
 
 
+axios.defaults.baseURL = 'https://api.unsplash.com';
+// axios.defaults.headers.common['Authorization'] = "client_id=RjIsPZVdJdHA7EIX1kcSuYMG_YljaeuXcwVRHctfYpI&query";
+const accessKey = 'RjIsPZVdJdHA7EIX1kcSuYMG_YljaeuXcwVRHctfYpI&query';
+
+// axios.get('https://api.unsplash.com/photos', {
+//     headers: {
+//         Authorization: `Client-ID ${accessKey}`
+//     }
+// })
+// .then(response => {
+//     console.log(response.data);
+// })
+// .catch(error => {
+//     console.error('Ошибка:', error);
+// });
+
+
+
+
+
  const fetchArticlesWithTopic = async topic => {
-  const response = await axios.get(`https://api.unsplash.com/search/photos/?client_id=RjIsPZVdJdHA7EIX1kcSuYMG_YljaeuXcwVRHctfYpI&query=${topic}`);
+  const response = await axios.get(`/search/photos/?query=${topic}`,{
+    headers: {
+        Authorization: `Client-ID ${accessKey}`
+    }
+});
   // const response = await axios.get(`https://api.unsplash.com/search/photos/?client_id=RjIsPZVdJdHA7EIX1kcSuYMG_YljaeuXcwVRHctfYpI&query=${topic}`);
   return response.data;
 };
